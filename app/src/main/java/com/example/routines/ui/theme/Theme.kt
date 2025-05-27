940fb886cac56bb58882a1b74812a3346a203b96
johnyjosh <johnyjosh@hotmail.com>
2025-05-26 21:55:46 -0700

    fix warnings

--- app/src/main/java/com/example/routines/ui/theme/Theme.kt
+++ app/src/main/java/com/example/routines/ui/theme/Theme.kt
@@ -1,14 +1,7 @@
 package com.example.routines.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
